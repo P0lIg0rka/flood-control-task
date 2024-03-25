fd4501eba5f263a32b4a96bbcba3f8c76b466ab7
P0lIg0rka <ivpolezhaev@edu.hse.ru>
2024-03-25 10:53:58 +0300

    Final answer

---
+++ fc/fc.go
@@ -0,0 +1,58 @@
+package fc
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+)
+
+type userInfo struct {
+	queue []int
+	done  chan struct{}
+}
+
+type FloodControler struct {
+	mu        *sync.Mutex
+	n, k      int
+	controler map[int64]*userInfo
+}
+
+func (fc FloodControler) Check(ctx context.Context, userID int64) (bool, error) {
+	select {
+	case <-ctx.Done():
+		return false, ctx.Err()
+	default:
+		if fc.n >= 0 && fc.k >= 0 {
+			return false, errors.New("n and k cannot be negative")
+		}
+
+		now := time.Now().Second()
+		// Получение элемента из мапы залочил, так как канал нужно создать при первом запросе от пользователя
+		// А без мьютекса может возникнуть data race
+		fc.mu.Lock()
+		nowUser, ok := fc.controler[userID]
+		if !ok {
+			nowUser = &userInfo{done: make(chan struct{})}
+		}
+		fc.mu.Unlock()
+
+		nowUser.done <- struct{}{}
+		defer func() {
+			<-nowUser.done
+		}()
+
+		// Сразу добавил элемент в конец очереди, чтобы не делать доп проверку на число элементов в массиве в условии цикла
+		// Проверка внутри цикла так же не нужна так как последний элемент = now, a now - now = 0, что не может быть больше чем fc.n
+		// Значит в цикл я зайду с хотя бы двумя элементами в слайсе
+		nowUser.queue = append(nowUser.queue, now)
+		for now-nowUser.queue[0] > fc.n {
+			nowUser.queue = nowUser.queue[1:]
+		}
+
+		if len(nowUser.queue) >= fc.k {
+			return true, nil
+		}
+		return false, nil
+	}
+}
--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
-	"sync"
-	"time"
 )
 
 func main() {
@@ -17,49 +14,4 @@ type FloodControl interface {
 	// Check возвращает false если достигнут лимит максимально разрешенного
 	// кол-ва запросов согласно заданным правилам флуд контроля.
 	Check(ctx context.Context, userID int64) (bool, error)
-}
-
-type userInfo struct {
-	queue []int
-	done  chan struct{}
-}
-
-type FloodControler struct {
-	mu        *sync.Mutex
-	n, k      int
-	controler map[int64]*userInfo
-}
-
-func (fc FloodControler) Check(ctx context.Context, userID int64) (bool, error) {
-	select {
-	case <-ctx.Done():
-		return false, ctx.Err()
-	default:
-		if fc.n >= 0 && fc.k >= 0 {
-			return false, errors.New("n and k cannot be negative")
-		}
-
-		now := time.Now().Second()
-		fc.mu.Lock()
-		nowUser, ok := fc.controler[userID]
-		if !ok {
-			nowUser = &userInfo{done: make(chan struct{})}
-		}
-		fc.mu.Unlock()
-		nowUser.done <- struct{}{}
-		nowUser.queue = append(nowUser.queue, now)
-		// Сразу добавил элемент в конец очереди, чтобы не делать доп проверку на число элементов в массиве в условии цикла
-		// Проверка внутри цикла так же не нужна так как последний элемент = now, a now - now = 0, что не может быть больше чем fc.n
-		// Значит в цикл я зайду с хотя бы двумя элементами в слайсе
-		for now-nowUser.queue[0] > fc.n {
-			nowUser.queue = nowUser.queue[1:]
-		}
-
-		if len(nowUser.queue) >= fc.k {
-			<-nowUser.done
-			return true, nil
-		}
-		<-nowUser.done
-		return false, nil
-	}
 }
