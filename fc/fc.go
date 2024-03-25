066a44dfe45f0b8f6f1d565f11bc9af4992c7516
P0lIg0rka <ivpolezhaev@edu.hse.ru>
2024-03-25 20:51:07 +0300

    hope reallocs will not destroy programm because of pointers

--- fc/fc.go
+++ fc/fc.go
@@ -3,7 +3,6 @@ package fc
 import (
 	"context"
 	"errors"
-	"sync"
 	"time"
 )
 
@@ -13,7 +12,7 @@ type userInfo struct {
 }
 
 type FloodControler struct {
-	mu        sync.Mutex
+	mu        chan struct{}
 	n, k      int
 	controler map[int64]*userInfo
 }
@@ -28,24 +27,27 @@ func (fc FloodControler) Check(ctx context.Context, userID int64) (bool, error) 
 		}
 
 		now := time.Now().Second()
+
 		// Получение элемента из мапы залочил, так как канал нужно создать при первом запросе от пользователя
-		// А без мьютекса может возникнуть data race
-		fc.mu.Lock()
+		// А канала будет возникать data race и так как в каналах поддерживается принцип FIFO, то те горутины, которые пришли раньше - пройдут дальше тоже раньше
+		fc.mu <- struct{}{}
 		nowUser, ok := fc.controler[userID]
 		if !ok {
 			nowUser = &userInfo{done: make(chan struct{}, 1)}
 		}
-		fc.mu.Unlock()
 
 		nowUser.done <- struct{}{}
 		defer func() {
 			<-nowUser.done
 		}()
 
+		<-fc.mu
+		// После этого в map не идёт новых записей, так что она не сможет реалоцировать, и для чтения можно дальше обращаться без опаски
+		// А вот запись для каждого userID будет происходить в той очерёдности, в которой они пришли
+
 		// Сразу добавил элемент в конец очереди, чтобы не делать доп проверку на число элементов в массиве в условии цикла
 		// Проверка внутри цикла так же не нужна так как последний элемент = now, a now - now = 0, что не может быть больше чем fc.n
 		// Значит в цикл я зайду с хотя бы двумя элементами в слайсе
-		nowUser = fc.controler[userID]
 		nowUser.queue = append(nowUser.queue, now)
 		for now-nowUser.queue[0] > fc.n {
 			nowUser.queue = nowUser.queue[1:]
@@ -61,6 +63,7 @@ func CreateFC(n int, k int) *FloodControler {
 	return &FloodControler{
 		n:         n,
 		k:         k,
+		mu:        make(chan struct{}, 1),
 		controler: make(map[int64]*userInfo),
 	}
 }
